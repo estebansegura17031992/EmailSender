2acce13933b4e92cb50eafc45c453ae179f941a2
Esteban Segura Benavides <esteban.1703@gmail.com>
2019-04-29 10:30:57 -0600

    [UPDATE] Se envia el correo por email via sendgrid

--- EmailSender/Controllers/HomeController.cs
+++ EmailSender/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
-﻿using EmailSender.Models;
+﻿using EmailSender.Helpers;
+using EmailSender.Models;
 using EmailSenderDAO;
 using EmailSenderDAO.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -29,9 +31,11 @@ namespace EmailSender.Controllers
             return Json(mails, JsonRequestBehavior.AllowGet);
         }
 
-        public ActionResult SendEmail(EmailModel email)
+        public async Task<ActionResult> SendEmail(EmailModel email)
         {
             //1. Envio de Correo con Sendgrid
+            EmailHelper emailHelper = new EmailHelper();
+            await emailHelper.SendGeneralEmail(email.Email, email.Subject, email.Body);
 
             //2. Almacenar datos del correo
             Mail mail = new Mail();
---
+++ EmailSender/Helpers/EmailHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+namespace EmailSender.Helpers
+{
+    public class EmailHelper
+    {
+        public async Task SendGeneralEmail(string ToEmail, string Subject, string Body)
+        {
+            var apiKey = ConfigurationManager.AppSettings["ApiKeySendGRID"];
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("esteban.1703@hotmail.com", "Esteban Segura Beanvides SnowCapCR");
+            var to = new EmailAddress(ToEmail, "Example User: "+ToEmail);
+            var msg = MailHelper.CreateSingleEmail(from, to, Subject, Body,"");
+            var response = await client.SendEmailAsync(msg);
+        }
+    }
+}
